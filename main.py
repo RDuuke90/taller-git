429fd3af9bd43aa750b2ad050d8474403ea4e973
RDuuke90 <115588732+RDuuke90@users.noreply.github.com>
2024-10-28 17:22:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,6 @@
 5-> Agregando los archivos al flujo para subir -> git add . | git add main.py 
 6-> Creando un punto de guardado(commit) -> git commit -m "Descripcion del guardado"
 7-> Subiendo el commit al remoto -> git push origin main | git push <origen> <rama>
-
 '''
 
+print("Hola mundo")
