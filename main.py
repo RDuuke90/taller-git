2c7899b882a01e4c7faed02dcebe3fde9e9b6a90
Juan Duque <jduqueg2@correo.tdea.edu.co>
2024-10-28 17:18:29 -0500

    Main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+'''
+1-> Iniciar respositorio -> git init
+2-> Ver en que rama estoy -> git branch
+3-> Definir una rama/Renombrar una rama -> git branch -M main
+4-> Agregando un repositorio remoto -> git remote add origin https://github.com/RDuuke90/taller-git.git
+5-> Agregando los archivos al flujo para subir -> git add . | git add main.py 
+6-> Creando un punto de guardado(commit) -> git commit -m "Descripcion del guardado"
+7-> Subiendo el commit al remoto -> git push origin main | git push <origen> <rama>
+
+notas: Agregando perfil 
+git config user.email "jduqueg2@correo.tdea.edu"
+git config user.name "Juan Duque"
+'''
