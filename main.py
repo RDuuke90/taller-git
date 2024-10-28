4258c931a839086a6fea314273e250fcac45dd66
Juan Duque <jduqueg2@correo.tdea.edu.co>
2024-10-28 17:27:17 -0500

    Agregando for en main

--- main.py
+++ main.py
@@ -6,6 +6,12 @@
 5-> Agregando los archivos al flujo para subir -> git add . | git add main.py 
 6-> Creando un punto de guardado(commit) -> git commit -m "Descripcion del guardado"
 7-> Subiendo el commit al remoto -> git push origin main | git push <origen> <rama>
+8-> Obtener cambios del gitremoto -> git pull origin main | git push <origen> <rama>
+
+9->Crear una nueva rama -> git branch feature | git branch <nombre>
+10->Cambiar de rama -> git checkout feature | git checkout <nombre>
 '''
 
 print("Hola mundo")
+for n in range[1:10]:
+    print(n)
